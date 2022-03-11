e91766cdfa7a7c922ddff995f352eb13503a7fd9
Basile REGNEAU <bregneau@student.42.fr>
2022-03-11 19:57:39 +0100

    WIP

---
+++ includes/structs.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 12:41:08 by bregneau          #+#    #+#             */
+/*   Updated: 2022/03/11 18:10:56 by bregneau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+/*Operators
+&
+|
+<
+>
+*/
+
+typedef struct s_env
+{
+	char			*var;
+	struct s_env	*next;
+}	t_env;
+
+typedef enum type
+{
+	WORD,
+	ASSIGNMENT_WORD,
+	NAME,
+	NEWLINE,
+	IO_NUMBER,
+	AND_IF = '&&',
+	OR_IF = '||',
+	DLESS = '<<',
+	DGREAT = '>>',
+}	t_type;
+
+typedef struct s_token
+{
+	char			*word;
+	t_type			type;
+	struct s_token	*next;
+}	t_token;
+
+
+typedef struct s_data
+{
+	t_env	*env;
+	t_token	*tok;
+}	t_data;
+
+#endif
