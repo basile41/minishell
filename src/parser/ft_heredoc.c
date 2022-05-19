9a6391d73f6bd6cffd80b1ac7d29c6938d8399b3
Basile REGNEAU <bregneau@student.42.fr>
2022-05-19 19:05:28 +0200

    WIP rework heredoc

--- src/parser/ft_heredoc.c
+++ src/parser/ft_heredoc.c
@@ -6,26 +6,70 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 19:13:28 by bregneau          #+#    #+#             */
-/*   Updated: 2022/04/01 19:14:07 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/05/19 19:03:37 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_create_tmp_file(char *here_doc)
+{
+	static size_t	i;
+	int				fd;
+	char			*tmp_file;
+
+	tmp_file = ft_itoa(i);
+	if (tmp_file)
+		tmp_file = ft_strjoin("/tmp/-ms-thd-", tmp_file);
+	if (tmp_file == NULL)
+		ft_exit_perror("malloc");
+	fd = open(tmp_file, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
+	if (fd == -1)
+		ft_exit_perror("open");
+	write(fd, here_doc, ft_strlen(here_doc));
+	close(fd);
+	fd = open(tmp_file, O_RDONLY);
+	if (fd == -1)
+		ft_exit_perror("open");
+	unlink(tmp_file);
+	i++;
+	return (fd);
+}
+
 int	ft_heredoc(char	*delimiter)
 {
 	char	*line;
-	int		pipefd[2];
+	char	*here_doc;
+	int		fd;
 
-	pipe(pipefd);
+	here_doc = NULL;
 	line = readline("heredoc> ");
 	while (line && ft_strcmp(line, delimiter))
 	{
-		write(pipefd[1], line, ft_strlen(line));
+		here_doc = ft_add_to_str(here_doc, line, ft_strlen(line));
 		free(line);
 		line = readline("heredoc> ");
 	}
 	free(line);
-	close(pipefd[1]);
-	return (pipefd[0]);
+	fd = ft_create_tmp_file(here_doc);
+	free(here_doc);
+	return (fd);
 }
+
+// int	ft_heredoc(char	*delimiter)
+// {
+// 	char	*line;
+// 	int		pipefd[2];
+
+// 	pipe(pipefd);
+// 	line = readline("heredoc> ");
+// 	while (line && ft_strcmp(line, delimiter))
+// 	{
+// 		write(pipefd[1], line, ft_strlen(line));
+// 		free(line);
+// 		line = readline("heredoc> ");
+// 	}
+// 	free(line);
+// 	close(pipefd[1]);
+// 	return (pipefd[0]);
+// }
