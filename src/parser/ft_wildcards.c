9de7ee51c95f05cd2ba112bd4db432ac0546e430
Caroline MARION <cmarion@e2r9p20.clusters.42paris.fr>
2022-05-24 09:58:36 +0200

    wildcards ip

--- src/parser/ft_wildcards.c
+++ src/parser/ft_wildcards.c
@@ -6,7 +6,7 @@
 /*   By: cmarion <cmarion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 14:27:30 by cmarion           #+#    #+#             */
-/*   Updated: 2022/05/23 18:00:12 by cmarion          ###   ########.fr       */
+/*   Updated: 2022/05/24 09:48:12 by cmarion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,14 @@ char	**w_content(char **content, char *str, int i, char **ret)
 		while (content[j][++ k])
 		{
 			l = 0;
-			while (content[j][k + l] == str[l] && l <= i)
+			while (content[j][k + l] == str[l] && l < i)
 				l ++;
 			if (l == i)
 			{	
 				ret_size = tabchar_len(ret);
-				ret = ft_realloc(ret, (ret_size + 1) * sizeof(char *),
-						(ret_size + 2) * sizeof(char *));
+				ret = ft_realloc(ret, ret_size * sizeof(char *),
+						(ret_size + 1) * sizeof(char *));
 				ret[ret_size] = ft_strdup(content[j]);
-				printf("content%d] = %s\n", k, content[j]);
 				ret[ret_size + 1] = NULL;
 			}
 		}
@@ -64,17 +63,17 @@ char	**w_ending(char **content, char *str, char **ret)
 	i = -1;
 	while (content[++ i])
 	{
-		j = ft_strlen(content[i]);
-		k = ft_strlen(str);
-		while (content[i][j --] == str[k --])
+		j = ft_strlen(content[i]) + 1;
+		k = ft_strlen(str) + 1;
+		while (content[i][-- j] == str[-- k])
 			;
-		if (k == 0)
+		if (content[i][j + 1] == str[k + 1] && (k == -1 || str[k] == '*'))
 		{
 			ret_size = tabchar_len(ret);
 			ret = ft_realloc(ret, ret_size * sizeof(char *),
-					ret_size * sizeof(char *) + 1);
-			ret[ret_size + 1] = ft_strdup(content[i]);
-			ret[ret_size + 2] = NULL;
+					(ret_size + 1) * sizeof(char *));
+			ret[ret_size] = ft_strdup(content[i]);
+			ret[ret_size + 1] = NULL;
 		}
 	}
 	return (ret);
@@ -98,7 +97,6 @@ char	**w_begining(char **content, char *str, int i, char **ret)
 			ret = ft_realloc(ret, ret_size * sizeof(char *),
 					(ret_size + 1) * sizeof(char *));
 			ret[ret_size] = ft_strdup(content[j]);
-			printf("ret[%d] = %s\n", ret_size, ret[ret_size]);
 			ret[ret_size + 1] = NULL;
 		}
 	}
@@ -110,18 +108,24 @@ char	**complex_star(char *str, char **content)
 	int		i;
 	char	**ret;
 
-	ret = calloc(sizeof(char *), 0);
+	ret = ft_calloc(1, sizeof(char *));
 	i = -1;
 	while (str[++ i])
 	{
 		if (str[i] == '*')
 		{
-			if (star_before(str, i - 1) > 0 && star_after(&str[i + 1]) > 0)
-				w_content(content, &str[star_before(str, i)], star_after(&str[i]), ret);
+			if (star_after(&str[i + 1]) > 0)
+			{
+				ret = w_content(content, &str[i + 1], star_after(&str[i + 1]), ret);
+				i = i + star_after(&str[i + 1]);
+			}
+			if (star_before(str, i - 1) == 0 && str[i - 1])
+				ret = w_begining(content, str, i, ret);
 			if (str[i + 1] && star_after(&str[i + 1]) == 0)
-				w_ending(content, &str[i], ret);
-			if (star_before(str, i - 1) == 0 && !str[i + 1])
-				w_begining(content, str, i, ret);
+			{
+				ret = w_ending(content, &str[i + 1], ret);
+				break ;
+			}
 		}
 	}
 	i = -1;
@@ -155,6 +159,9 @@ char	**ft_wildcard(char *wild)
 	free (dir);
 	free (content);
 	expwild[size - 1] = NULL;
+	printf("************\n");
+	aff_tabcchar(expwild);
+	printf("************\n");
 	if (wild[0] == '*' && !wild[1])
 		return (expwild);
 	return (complex_star(wild, expwild));
--- src/utils/ft_wildcards_utils.c
+++ src/utils/ft_wildcards_utils.c
@@ -6,7 +6,7 @@
 /*   By: cmarion <cmarion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 10:55:44 by cmarion           #+#    #+#             */
-/*   Updated: 2022/05/23 16:53:08 by cmarion          ###   ########.fr       */
+/*   Updated: 2022/05/24 09:27:02 by cmarion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	star_before(char *str, int i)
 {
 	while (str[i] && str[i] != '*' && i > 0)
 		i --;
-	if (str[i] == '*')
+	if (str[i] && str[i] == '*')
 		return (i);
 	return (0);
 }
@@ -34,11 +34,15 @@ int	star_before(char *str, int i)
 int	star_after(char *str)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while (str[i] == '*')
+	while (str[i] && str[i] == '*')
 		i ++;
-	if (str[i] == '*')
-		return (i);
+	j = 0;
+	while (str[i + j] && str[i + j] != '*')
+		i ++;
+	if (str[i + j] && str[i + j] == '*')
+		return (j);
 	return (0);
 }
